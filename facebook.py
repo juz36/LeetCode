810d7b43d824064fe861c67c3ad5ccbc7ed523d8
juz36 <33359228+juz36@users.noreply.github.com>
2018-09-24 21:38:45 -0400

    Add new solution

--- facebook.py
+++ facebook.py
@@ -5,6 +5,22 @@ class Solution(object):
             self.left = None
             self.right = None
 
+    def longestConsecutive(self, root):
+        if not root:
+            return 0
+            
+        ret = 0
+        stack = [(root, 1)]
+        while stack:
+            node, cnt = stack.pop()
+            if node.left:
+                stack.append((node.left, cnt+1 if node.left.val == node.val + 1 else 1))
+            if node.right:
+                stack.append((node.right, cnt+1 if node.right.val == node.val + 1 else 1))
+            ret = max(ret, cnt)
+            
+        return ret
+
     def isSubtree(self, s, t):
         """LeetCode Subtree of another Tree"""
         def isMatch(s, t):
